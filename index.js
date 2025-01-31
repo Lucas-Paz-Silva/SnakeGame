9b515f5e545089cbf50bb0b941c33f249ff409e2
Lucas-Paz-Silva <112334349+Lucas-Paz-Silva@users.noreply.github.com>
2025-01-31 17:12:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -79,3 +79,4 @@ document.addEventListener('keydown', event => {
     const keyMap = { ArrowUp: 'up', ArrowDown: 'down', ArrowLeft: 'left', ArrowRight: 'right' };
     if (keyMap[event.key]) changeDirection(keyMap[event.key]);
 });
+
